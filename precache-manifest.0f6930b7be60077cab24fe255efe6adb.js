2ebec010626ec333db750b8b4cc20e46100c48b1
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-03-03 12:02:33 +0100

    Updates

---
+++ precache-manifest.0f6930b7be60077cab24fe255efe6adb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96fd133ad39bf1c1e711",
+    "url": "/react-game/static/css/main.a87bb0cc.chunk.css"
+  },
+  {
+    "revision": "96fd133ad39bf1c1e711",
+    "url": "/react-game/static/js/main.519dafc2.chunk.js"
+  },
+  {
+    "revision": "3a038c117699a7a341e8",
+    "url": "/react-game/static/js/runtime~main.ef933c85.js"
+  },
+  {
+    "revision": "946507ca19d70d749c94",
+    "url": "/react-game/static/js/2.40ad7508.chunk.js"
+  },
+  {
+    "revision": "11d6ce16f5997b072d147b84392226e8",
+    "url": "/react-game/index.html"
+  }
+];
