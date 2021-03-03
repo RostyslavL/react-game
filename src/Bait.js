4961c6ad990addc8c7e2615c26ef6987c696e858
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-03-03 11:46:18 +0100

    feat: implemented  randob color function for component

--- src/Bait.js
+++ src/Bait.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+// https://stackoverflow.com/questions/10014271/generate-random-color-distinguishable-to-humans
+const getRandomColor = () =>{
+    var letters = '0123456789ABCDEF'
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color;
+  }
 const Bait = (props) => {
     const style = {
         left: `${props.dot[0]}%`,
         top: `${props.dot[1]}%`,
-    
+        backgroundColor : getRandomColor()
       }
     
       return (
